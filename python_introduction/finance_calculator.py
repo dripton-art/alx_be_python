2f6baece9d22b8db6516b75d950e958863b87c2f
ivan <ivankaweesa.biz@gmail.com>
2025-05-27 00:35:34 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,5 +7,6 @@ Annual Savings = Monthly Savings * 12
 
 # Assume a simple annual interest rate of 5%
 Projected Savings =  Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+
 print(f"Monthly Savings are: $[Monthly Savings:.2f]")
 print(f"Projected savings after one year, with interest, is: $[Projected Savings:.2f]")
