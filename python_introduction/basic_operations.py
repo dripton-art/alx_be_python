1e2ed6c2598ea6e3ca33ac0c52d4a4c5ea85115f
ivan kaweesa <lilswings7@gmail.com>
2025-05-26 20:21:24 +0000

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+# Arithmetic operations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
