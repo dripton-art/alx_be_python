feedfd2684a19a6a4ac68c20d36787012335b569
ivan <ivankaweesa.biz@gmail.com>
2025-05-31 18:37:01 +0200

    Simple Calculator with Match Case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -9,15 +9,15 @@ result = num1 / num2
 
 match operation:
     case "+":
-        print("The result is {result}")
+        print(f"The result is {result}")
     case "-":
-        print("The result is {result}")
+        print(f"The result is {result}")
     case "*":
-        print("The result is {result}")
+        print(f"The result is {result}")
     case "/":
         if num2 == 0:
             print("cannot divide by zero.")
         else:
-            print("The result is {result}")
+            print(f"The result is {result}")
     case _:
         print("invalid operation!")
