17f5bf0601a3089d2c9cff91c28043ddc07c363f
ivan <ivankaweesa.biz@gmail.com>
2025-06-17 03:07:29 +0200

    A Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,7 +12,6 @@ class BankAccount:
             return True
         else:
             print("Insufficient funds.")
-            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
