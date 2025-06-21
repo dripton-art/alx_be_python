461218d0967b02f35be7a707b3cf60da277d388a
ivan <ivankaweesa.biz@gmail.com>
2025-06-21 21:39:22 +0200

    Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,39 +2,53 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.__is_checked_out = False
+        self._is_checked_out = False
 
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
 
-class Library():
-    """Library managment system"""
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+
+class Library:
     def __init__(self):
-        self._books = [] #Private list of books to be added here
+        self._books = []
 
     def add_book(self, book):
-        """Adding book to the library"""
-        self._books.append(book)        
+        if isinstance(book, Book):
+            self._books.append(book)
 
     def check_out_book(self, title):
-        """Check out books from the library"""
         for book in self._books:
-            if not book._Book__is_checked_out:
-                if  book.title == title:
-                    book._Book__is_checked_out = True
-                    return book
-             
+            if book.title == title and book.is_available():
+                if book.check_out():
+                    print(f"Book '{title}' has been checked out.")
+                    return
+        print(f"Book '{title}' is not available for checkout.")
+
     def return_book(self, title):
-        """Return a book to the library"""
         for book in self._books:
-            if book.title == title:
-                if book._Book__is_checked_out:
-                    book._Book__is_checked_out = False
-                    return book
-        return None
+            if book.title == title and not book.is_available():
+                if book.return_book():
+                    print(f"Book '{title}' has been returned.")
+                    return
+        print(f"Book '{title}' was not checked out or doesn't exist.")
 
     def list_available_books(self):
-        """List all available books in the library"""
-        for book in self._books:
-            if not book._Book__is_checked_out:
-                print(book)
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            print("Available books:")
+            for book in available_books:
+                print(f"- {book.title} by {book.author}")
+        else:
+            print("No books are currently available.")
