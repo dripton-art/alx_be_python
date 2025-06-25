75f425b024d1cf2643209ce2848d9302fd4f981a
ivan <ivankaweesa.biz@gmail.com>
2025-06-25 19:26:44 +0200

    Python Magic Methods

---
+++ oop/book_class.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title=str, author=str, year=int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        self.title = None
+        print("Deleting (title of the book)")
+
+    def __str__(self):
+        print("{title} by {author}, published in {year}")
+
+    def __repr__(self):
+        print(f"Book('{self.title}', '{self.author}', {self.year})")
+        
